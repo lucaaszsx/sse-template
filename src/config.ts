8ce27289dd806563473ef2973b8f635d9f1ec5e4
Lucas <jlucasmelob558@gmail.com>
2026-07-02 18:21:36 -0300

    chore: update config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,12 @@
+import type ms from 'ms';
 import 'dotenv/config';
 
 export const config = {
-    port: parseInt(process.env['SERVER_PORT'] as string ?? 3000, 10)
+    port: parseInt((process.env['SERVER_PORT'] as string) ?? 3000, 10),
+
+    // JWT
+    jwt: {
+        secret: (process.env['JWT_SECRET'] as string) ?? '',
+        expiration: process.env['JWT_EXP'] as ms.StringValue
+    }
 };
