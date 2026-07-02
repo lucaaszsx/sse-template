521408387a3c48f1e24b245390501e64932e1b55
Lucas <jlucasmelob558@gmail.com>
2026-07-02 18:24:00 -0300

    feat: make the routes public

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
+import { notificationRouter } from './routes/notification.js';
+import { authRouter } from './routes/auth.js';
+import { userRouter } from './routes/user.js';
 import { config } from './config.js';
 import express from 'express';
 import path from 'node:path';
+import { authorizationChecker } from './middlewares/authorizationChecker.js';
+import { currentUserChecker } from './middlewares/currentUserChecker.js';
 
-/** Server setup */
+// Server setup
 const app = express();
 app.use(express.static(path.resolve(import.meta.dirname, '..', 'public')));
+app.use(express.json());
 
-/** Routes */
-app.use();
+// Routes
+app.use('/api/auth', authRouter);
+app.use('/api/users', authorizationChecker, currentUserChecker, userRouter);
+app.use('/api/notifications', notificationRouter);
 
-/** Server initialization */
+// Server initialization
 app.listen(config.port, () => console.log(`[info] server listening at localhost:${config.port}`));
